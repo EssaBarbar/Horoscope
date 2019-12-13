57a145fd73a2f33635950f8300e90c25ee4fc5fe
Essa <essa992@gmail.com>
2019-12-13 09:20:31 +0100

    added html content

---
+++ addHoroscope.php
@@ -0,0 +1,6 @@
+<?php
+$Horoskops =array(array(array('Name' => 'Jungfru', 'Month1' => 'augusti', 'Month2' => 'September')
+)
+);
+error_log($Horoskops[1][1][1])
+?>
--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 window.onload = main
 
-function main() {
 
-}
+function main() {
+    addEventListner();
+    /* funktioernra som ska köras när sidan laddas */
+};
+
+function addEventListner() {
+    const saveThisDate = document.getElementById("saveButton");
+    saveThisDate.onclick = getDate;
+
+};
+
+function getDate() {
+    let dateOfBirth = document.getElementById("date").value;
+    let vals = dateOfBirth.split('-');
+    let year = vals[0];
+    let month = vals[1];
+    let day = vals[2];
+};
